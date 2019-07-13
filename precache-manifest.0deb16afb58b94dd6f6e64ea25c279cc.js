89f8462b39864262dcd2ff14fa52f251fec9712e
andregtz <colpac64@gmail.com>
2019-07-12 19:41:40 -0500

    Nueva pagina

---
+++ precache-manifest.0deb16afb58b94dd6f6e64ea25c279cc.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f6d0e641e22c916ad9c9b607fb2c944",
+    "url": "/img/img-home-operators.jpg"
+  },
+  {
+    "revision": "770e380f4c47b13b7a9d68634395a8eb",
+    "url": "/img/machinery/Excavadora.png"
+  },
+  {
+    "revision": "a7edb6cb572f99f07227",
+    "url": "/js/chunk-2d2311a9.2097a9cd.js"
+  },
+  {
+    "revision": "b1a90036fd9c3db09e36",
+    "url": "/js/chunk-2d23777f.cfc24d52.js"
+  },
+  {
+    "revision": "8d17515d84507ef2bb0d",
+    "url": "/js/chunk-3de72a1c.9bc2d191.js"
+  },
+  {
+    "revision": "e7202d044452779b5d2de623e9800382",
+    "url": "/img/machinery/Titan 15 ton 21m.png"
+  },
+  {
+    "revision": "9b5005833aa0f8f9ec3b",
+    "url": "/js/chunk-4a2d375b.8918ae1f.js"
+  },
+  {
+    "revision": "1f1efd2a09a778a305a1",
+    "url": "/js/chunk-vendors.4ab0c126.js"
+  },
+  {
+    "revision": "2cec4df0e0ca3af04061",
+    "url": "/js/contact.365e0755.js"
+  },
+  {
+    "revision": "9e983484a6baeb147945",
+    "url": "/js/machines.1f099a3c.js"
+  },
+  {
+    "revision": "20dd200a01812ae82f08d7706978452f",
+    "url": "/img/machinery/Grua30.png"
+  },
+  {
+    "revision": "b5f11e8d5214c2184d9cafaab4ea9489",
+    "url": "/img/machinery/Grua 5 Ton.png"
+  },
+  {
+    "revision": "e606767d9ab8c9d841836e191c19f81e",
+    "url": "/img/machinery/Retroexcavadora.png"
+  },
+  {
+    "revision": "2c6398d5a66a7e2eca49b6f905b29368",
+    "url": "/img/machinery/Grua 2 4.9 ton.png"
+  },
+  {
+    "revision": "02229e74db30f1af4fcab4ee93599715",
+    "url": "/img/machinery/Grua 4.9 ton.png"
+  },
+  {
+    "revision": "dcf266edde8281501f271dbee3561e7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "343b060c90dba0d16b8801cdc98174c8",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f7ee4a63c80f79fe16457be89e195688",
+    "url": "/sitemap.xml"
+  },
+  {
+    "revision": "b43bed4d7f43e43f3fc20a9ef62a613f",
+    "url": "/img/machinery/Grua 100 ton.png"
+  },
+  {
+    "revision": "791388595d472d8e79f2832a359a7f45",
+    "url": "/img/machinery/Titan7.png"
+  },
+  {
+    "revision": "4dbbf06a7a7623fb8ce025eabbfebd5a",
+    "url": "/img/machinery/Komatsu.png"
+  },
+  {
+    "revision": "cfdc652bc2cfbbcbed59",
+    "url": "/js/app.262ce5c2.js"
+  },
+  {
+    "revision": "4b4ee5b9d9cdc0676124bfc9951b3eae",
+    "url": "/img/machinery/Grua 45 ton.png"
+  },
+  {
+    "revision": "173462408d72ee10f231ce106318b692",
+    "url": "/img/machinery/Grua200.png"
+  },
+  {
+    "revision": "b5df0782d3670771898e5bb14f952f01",
+    "url": "/img/machinery/Titan1.png"
+  },
+  {
+    "revision": "8ca6e11f9185e1075f4680453a35d3c4",
+    "url": "/img/machinery/Grua18.png"
+  },
+  {
+    "revision": "6c35e31abe14486725a89247fc6b20e7",
+    "url": "/img/headers/machines-mask.jpg"
+  },
+  {
+    "revision": "c93843ebcc1f8f9284b353ac6970a40d",
+    "url": "/img/headers/machines.jpg"
+  },
+  {
+    "revision": "d61652f114956ba803968654a192337f",
+    "url": "/img/img-machinery-polygon.jpg"
+  },
+  {
+    "revision": "69c66b94f6f1b0a6738956250337fd24",
+    "url": "/img/img-home-polygon.jpg"
+  },
+  {
+    "revision": "50dfeaf8fc52409d9e81f9566f78b089",
+    "url": "/img/img-callToAction.png"
+  },
+  {
+    "revision": "8b29d443c900c53217351f3ccfe7e610",
+    "url": "/img/img-contact-polygon.jpg"
+  },
+  {
+    "revision": "a5a6ded1c49f5dc2e7236c2aff69e0d5",
+    "url": "/fonts/Regular.a5a6ded1.otf"
+  },
+  {
+    "revision": "cb15bbe090e7ddedcdbfa905bb8190bf",
+    "url": "/img/headers/home-mask.jpg"
+  },
+  {
+    "revision": "7ef2c5af2abdfc564bf7a908ce8a7fb4",
+    "url": "/img/headers/home.jpg"
+  },
+  {
+    "revision": "4354988a45f1344fe90f0a7e8af2ea3f",
+    "url": "/img/headers/contact.jpg"
+  },
+  {
+    "revision": "cba862045d471769f7625f98e51b36fb",
+    "url": "/img/headers/contact-mask.jpg"
+  },
+  {
+    "revision": "0ae768cafe716ef210d6624b623ae935",
+    "url": "/fonts/Regular.0ae768ca.otf"
+  },
+  {
+    "revision": "f2c3f4ddac4200dcd9cb638f2e4de5d0",
+    "url": "/fonts/Bold.f2c3f4dd.otf"
+  },
+  {
+    "revision": "be16a1d0e74780342b0d8ed9a84f525e",
+    "url": "/fonts/Bold.be16a1d0.otf"
+  },
+  {
+    "revision": "9b5005833aa0f8f9ec3b",
+    "url": "/css/chunk-4a2d375b.79b6201e.css"
+  },
+  {
+    "revision": "cfdc652bc2cfbbcbed59",
+    "url": "/css/app.bb3ae7c2.css"
+  },
+  {
+    "revision": "926325887b7ca22da26d60fcd8c78ce5",
+    "url": "/app.html"
+  }
+];
